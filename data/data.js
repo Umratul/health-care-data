1c4c9897e7a1c622e0bd0ff056f3f3f89ef316b6
Umratul <umratul0904@gmail.com>
2021-10-20 21:02:26 +0600

    second

--- data/data.js
+++ data/data.js
@@ -3,38 +3,38 @@ const details = [
         id: 01,
         name: 'Teeth Extraction',
         img: 'https://costellodentistry.ca/wp-content/uploads/2020/12/shutterstock_1305962044.jpg',
-        body: ''  
+        body: 'Tooth extraction refers to the complete removal of one or more teeth from the mouth. This is a procedure that is usually performed by a dental surgeon. Milk teeth may be removed from a child mouth without intervention from the dentist as they loosen as a matter of course to give way to permanent teeth.'  
     },
     {
         id: 02,
         name: 'Teeth Filling',
         img: 'https://img.freepik.com/free-vector/teeth-model-mirror_1308-33381.jpg?size=338&ext=jpg',
-        body: ''  
+        body: 'Fillings are most commonly used to prevent further damage to a tooth when decay is present. By removing the bacteria and closing the affected area off, no further tooth decay can occur.'  
     },
     
     {
         id: 03,
         name: 'Braces Treatment',
         img: 'https://image.freepik.com/free-vector/woman-smile-with-braces-isolated-beige_133260-442.jpg',
-        body: ''  
+        body: 'Braces are most often used during adolescence, but more and more adults are getting corrective dental braces later in life.Braces are made of metal or ceramic, wires, and bonding material that attaches them to your teeth. An orthodontist is a doctor who specializes in this kind of device and treatment for misaligned teeth.'  
     },
     {
         id: 04,
         name: 'Laser Treatment',
         img: 'https://image.freepik.com/free-vector/clean-dirty-tooth-whitening-clearing-teeth_107791-2298.jpg',
-        body: ''
+        body: 'Lasers are used to remove decay within a tooth and prepare the surrounding enamel for receipt of the filling. Gum disease. Lasers are used to reshape gums and remove bacteria during root canal procedures'
     },
     {
         id: 05,
         name: 'Root Canal',
         img: 'https://image.freepik.com/free-vector/enamel-caries-infection-health-tooth_81522-103.jpg',
-        body: ''  
+        body: 'Root canal treatment (endodontics) is a dental procedure used to treat infection at the centre of a tooth. Root canal treatment is not painful and can save a tooth that might otherwise have to be removed completely.'  
     },
     {
         id: 06,
         name: 'Parmanent Denture',
         img: 'https://www.boroondaradental.com.au/images/content/thumbnail_Implants2.jpg',
-        body: '',
+        body: 'Permanent dentures are an alternative to removable dentures for missing teeth. Unlike traditional dentures, permanent dentures are typically held in place by two or more surgically-placed dental implants, making them stable and allowing the patient to chew, talk, and smile more easily than with removable dentures.',
     }
     
 ]
