7a7e245d8010d4969560e4cd100f0c23af259a57
Umratul <umratul0904@gmail.com>
2021-10-20 13:13:47 +0600

    first commit

---
+++ data/data.js
@@ -0,0 +1,40 @@
+const details = [
+    {
+        id: 01,
+        name: 'Teeth Extraction',
+        img: 'https://costellodentistry.ca/wp-content/uploads/2020/12/shutterstock_1305962044.jpg',
+        body: ''  
+    },
+    {
+        id: 02,
+        name: 'Teeth Filling',
+        img: 'https://img.freepik.com/free-vector/teeth-model-mirror_1308-33381.jpg?size=338&ext=jpg',
+        body: ''  
+    },
+    
+    {
+        id: 03,
+        name: 'Braces Treatment',
+        img: 'https://image.freepik.com/free-vector/woman-smile-with-braces-isolated-beige_133260-442.jpg',
+        body: ''  
+    },
+    {
+        id: 04,
+        name: 'Laser Treatment',
+        img: 'https://image.freepik.com/free-vector/clean-dirty-tooth-whitening-clearing-teeth_107791-2298.jpg',
+        body: ''
+    },
+    {
+        id: 05,
+        name: 'Root Canal',
+        img: 'https://image.freepik.com/free-vector/enamel-caries-infection-health-tooth_81522-103.jpg',
+        body: ''  
+    },
+    {
+        id: 06,
+        name: 'Parmanent Denture',
+        img: 'https://www.boroondaradental.com.au/images/content/thumbnail_Implants2.jpg',
+        body: '',
+    }
+    
+]
